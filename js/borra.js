bffff56f6efc4004cf7c965cd912fcc2cb626309
roygual@hotmail.com <roygual@hotmail.com>
2015-03-25 17:11:24 -0600

    medio mapa

--- js/borra.js
+++ js/borra.js
@@ -19,25 +19,10 @@ function initialize() {
   service.nearbySearch(request, callback);
 }
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-}
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+polyline.setMap(map);
