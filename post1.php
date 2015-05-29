9586d64eeb2b6424e3742dfaa045ac092f4dc34b
roygual <roygual@hotmail.com>
2015-05-28 22:34:28 -0500

    retoque de catalogo y logo de soriana

--- post1.php
+++ post1.php
@@ -59,29 +59,3 @@
         
     }
         
-
-
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
-   
-?> 
